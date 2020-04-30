034d34bc429dbafca84f03a0ba55bc77f06de6ef
iuming <6ming@hrbeu.edu.cn>
2020-04-30 10:44:12 +0800

    open_lise++_13_4_4_beta

---
+++ CrossSections/PublishedData/136Xe_Al_760AMeV.cs
@@ -0,0 +1,15 @@
+! Cross sections of neutron-rich fragments produced in the reaction 760 A MeV 136Xe + 27Al
+! (from thesis Karl Zeitelhack, Fakultät für Physik der Technischen! 
+! Universität München, Physik Department E12, 1992).
+53      83      0.17       0.12
+52      83      4.79E-03   2.35E-03
+52      82      0.44       0.16
+51      81      5.61E-02   2.52E-02
+51      82      6.10E-03   2.93E-03
+50      79      3.85E-02   1.93E-02
+50      80      9.85E-03   4.93E-03
+49      78      1.01E-02   4.90E-03
+48      76      5.48E-03   2.58E-03
+47      75      1.37E-03   6.90E-04
+46      73      6.38E-04   3.83E-04
+45      71      8.54E-04   5.98E-04
---
+++ CrossSections/PublishedData/40Ar_C_240AMeV.cs
@@ -0,0 +1,10 @@
+!  Isotpes of Sodium
+!   first five points   40Ar+C    Symons, PRL 42 (1979) 40
+!  24Na from 40Ar + Be (Notani thesis)
+
+11 15 	1.3	0.3
+11 16 	0.325	0.1
+11 17 	0.03	0.01
+11 18 	0.0065	0.002
+11 19 	0.0016	0.0015
+11 13 	10.3  0.5
